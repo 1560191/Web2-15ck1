fff56d7f64f9c6b3bb22b87ae9953ddbf95b49a7
1560155 <1560155@github.com>
2018-06-10 11:15:30 +0700

    Fix Giao diện

--- DoAnWeb2 - Copy/backend_src/fn/const.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PRODUCTS_PER_PAGE: 6
-}
-
-module.exports = {
-    PRODUCTS_PER_PAGE2: 5
-}
--- DoAnWeb2 - Copy/backend_src/repos/mail.js
+++
@@ -1,5 +0,0 @@
-var db = require('../fn/mysql-db');
-exports.update = function(email) {
-	var sql = `update taikhoan set Active = 1 where email = '${email}'`;
-	return db.load(sql);
-}
