7eabc1c4e1db7cecf0b56d70202f7b7b64e0af11
HuynhCongHau <31978939+HuynhCongHau@users.noreply.github.com>
2018-06-11 19:50:44 +0700

    Thêm thời gian đếm ngược

--- DoAnWeb2/client_src/assets/me/index.js
+++ DoAnWeb2/client_src/assets/me/index.js
@@ -46,19 +46,35 @@ $('#btnMore').on('click', function() {
 
 var loadProducts = function() {
     $('.loader').show();
-
     $.ajax({
         url: 'http://localhost:3000/sanpham?page=' + CUR_PAGE,
         dataType: 'json',
         timeout: 10000
     }).done(function(data) {
+
+        var aaas = "<script>\
+        var countDownDate = new Date('{{Thoigianketthuc}}').getTime();\
+        setInterval(function() {\
+        var now = new Date().getTime();\
+        var distance = countDownDate - now;\
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));\
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));\
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));\
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);\
+        document.getElementById({{ID}}).innerHTML = days + 'Ngày ' + hours + 'Giờ'+ minutes + 'Phút' + seconds + 'Giây'; });\
+        </script>\
+        {{/each}}";
+        //var source2 = $('#dest').html();
         var source = $('#product-template').html();
-        var template = Handlebars.compile(source);
+        var source3 =  source + aaas;
+        console.log(source3);
+        var template = Handlebars.compile(source3);
         var html = template(data.sanpham);
         $('#product-list').append(html);
 
         $('#product-list div[style]').fadeIn(200, function() {
             $(this).removeAttr('style');
+
         });
 
         CUR_PAGE++;
